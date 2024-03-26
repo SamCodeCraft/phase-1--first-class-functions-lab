b4d57b7db010bb131aba64c04a647ab8caacb0d3
siyajari samuel <siyajari.samuel@student.moringaschool.com>
2024-03-26 19:26:15 +0300

    Advanced Functions: First-class function

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2);
+}
+const returnLastTwoDrivers = function(drivers){
+   return drivers.slice(-2);
+}
+
+const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+console.log(returnFirstTwoDrivers(drivers));
+console.log(returnLastTwoDrivers(drivers));
+
+// array containing two elements (the first two previously defined functions)
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+ const createFareMultiplier = function(multiplier) {
+  return function(fare) {
+    return fare * multiplier;
+  };
+};
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+
+const selectDifferentDrivers = function(drivers, selectingFunction) {
+  return selectingFunction(drivers);
+};
+ 
+ 
+ 
+ 
+
+
+
+
+    
+
